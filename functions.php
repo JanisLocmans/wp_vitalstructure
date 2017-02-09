d069111721132c512d01fbaaa015c64eb1cf0124
john <j_locmans@gmail.com>
2017-02-09 20:08:26 +0200

    update: *-> added menu support. *-> added post-thumbnail support. *-> added widget support.

--- functions.php
+++ functions.php
@@ -5,16 +5,27 @@
  */
 /**
  * Set the content width based on the theme's design and stylesheet.
- *
- * @since VitalStructure 1.0
  */
 if ( ! isset( $content_width ) )
     $content_width = 920; /* pixels */
-
-
-if ( ! function_exists( 'vital_setup' ) ):
 /**
- * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * imports scripts and stylesheets.
+ *
+ * @since VitalStructure 1.0
+ */
+add_action( 'after_setup_theme', 'vital_setup' );
+
+function enque_files() {    
+    wp_enqueue_style('style', get_stylesheet_uri() );
+    wp_enqueue_script( 'script_js', get_template_directory_uri() . '/inc/js/scripts.js', 
+                        array(), 
+                        null, 
+                        true );
+}
+add_action('wp_enqueue_scripts', 'enque_files');
+/**
+ * Theme setup function
  *
  * Note that this function is hooked into the after_setup_theme hook, which runs
  * before the init hook. The init hook is too late for some features, such as indicating
@@ -22,17 +33,15 @@ if ( ! function_exists( 'vital_setup' ) ):
  *
  * @since VitalStructure 1.0
  */
+if ( ! function_exists( 'vital_setup' ) ):
 function vital_setup() {
  
     /**
-     * Custom template tags for this theme.
+     * require files
      */
     require( get_template_directory() . '/inc/template-tags.php' );
- 
-    /**
-     * Custom functions that act independently of the theme templates
-     */
     require( get_template_directory() . '/inc/tweaks.php' );
+    require( get_template_directory() . '/inc/customizer.php' );
  
     /**
      * Make theme available for translation
@@ -41,29 +50,53 @@ function vital_setup() {
      * to change 'vital' to the name of your theme in all the template files
      */
     load_theme_textdomain( 'vital', get_template_directory() . '/languages' );
- 
     /**
      * Add default posts and comments RSS feed links to head
      */
     add_theme_support( 'automatic-feed-links' );
- 
     /**
      * Enable support for the Aside Post Format
      */
     add_theme_support( 'post-formats', array( 'aside' ) );
- 
     /**
      * This theme uses wp_nav_menu() in one location.
      */
     register_nav_menus( array(
-       'primary' => __( 'Primary Menu', 'vital' ),
+       'Left' => __( 'Left', 'vital' ),
+        'Right' => __( 'right', 'vital' )
    	) );
+    /**
+     * add featured image support
+     */
+
+    add_theme_support('post-thumbnails');
+    add_image_size('entry-banner', 650, 366, true);
+
 }
 endif; // vital_setup
-add_action( 'after_setup_theme', 'vital_setup' );
 
-function enque_files() {	
-	wp_enqueue_style('style', get_stylesheet_uri() );
-	wp_enqueue_script( 'script_js', get_template_directory_uri() . '/inc/js/scripts.js', array(), null, true );
+/**
+ * Register widgetized area and update sidebar with default widgets
+ *
+ * @since VitalStructure 1.0
+ */
+function widget_area_init() {
+    register_sidebar( array(
+        'name' => __( 'Primary Widget Area', 'vital' ),
+        'id' => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
+ 
+    register_sidebar( array(
+        'name' => __( 'Secondary Widget Area', 'vital' ),
+        'id' => 'sidebar-2',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
 }
-add_action('wp_enqueue_scripts', 'enque_files');
+add_action( 'widgets_init', 'widget_area_init' );
