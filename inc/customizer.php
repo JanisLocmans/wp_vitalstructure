9c9fc01faf7cbe19445cfd8588cff38f1b3ee94a
john <j_locmans@gmail.com>
2017-02-09 20:17:36 +0200

    Update: *-> added Sidebar support *-> added customizer support * added Archive No-results Search

---
+++ inc/customizer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Main file for theme customization
+ *
+ * @package VitalStructure
+ * @since VitalStructure 1.0
+ */
+function vital_theme_customizer( $wp_customize ) {
+
+    $wp_customize->add_section( 'vital_logo_section' , array(
+    'title'       => __( 'Logo', 'vital' ),
+    'priority'    => 30,
+    'description' => 'Upload a logo to replace the default site name and description in the header',
+    ) );
+   $wp_customize->add_setting( 'vital_logo' );
+   $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'vital_logo', array(
+    'label'    => __( 'Logo', 'vital' ),
+    'section'  => 'vital_logo_section',
+    'settings' => 'vital_logo',
+    ) ) );
+
+   $wp_customize->add_section( 'vital_banner_section' , array(
+    'title'      => __('Banner','vital'),
+    'priority'   => 31,
+    'description' => 'Upload a banner',
+    ) );
+   $wp_customize->add_setting( 'vital_banner' );
+   $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'vital_banner', array(
+    'label'    => __( 'Banner', 'vital' ),
+    'section'  => 'vital_banner_section',
+    'settings' => 'vital_banner',
+    ) ) );
+
+
+}
+add_action('customize_register', 'vital_theme_customizer');
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,47 @@
-<div id="primary" class="content-area">
-    <div id="content" class="site-content" role="main">
-    </div><!-- #content .site-content -->
-</div><!-- #primary .content-area -->
+<?php
+/**
+* The Sidebar containing the main widget areas.
+*
+* @package Shape
+* @since Shape 1.0
+*/
+?>
+			
+
+
+
+<aside id="secondary" class="secondary-area" role="complementary">
+    <?php do_action( 'before_sidebar' ); ?>
+    <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+ 
+        <aside id="search" class="widget widget_search">
+            <?php get_search_form(); ?>
+        </aside>
+ 
+        <aside id="archives" class="widget">
+            <div class="widget-title"><h2><?php _e( 'Archives', 'shape' ); ?></h2></div>
+            <ul>
+                <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+            </ul>
+        </aside>
+ 
+        <aside id="meta" class="widget">
+            <div class="widget-title"><h2><?php _e( 'Meta', 'shape' ); ?></h2></div>
+            <ul>
+                <?php wp_register(); ?>
+                <li><?php wp_loginout(); ?></li>
+                <?php wp_meta(); ?>
+            </ul></aside>
+ 
+    <?php endif; // end sidebar widget area ?>
+
+    <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+ 
+     <div id="tertiary" class="widget-area" role="supplementary">
+      <?php dynamic_sidebar( 'sidebar-2' ); ?>
+     </div><!-- #tertiary .widget-area -->
+ 
+	<?php endif; ?>
+
+</aside><!-- #secondary .widget-area -->
+
